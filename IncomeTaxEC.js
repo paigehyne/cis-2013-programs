39c5dda7a3573064fbf1d578971272b816b58610
paigehyne <54879221+paigehyne@users.noreply.github.com>
2019-10-15 20:31:38 -0500

    Add files via upload

--- IncomeTaxEC.js
+++ IncomeTaxEC.js
@@ -34,7 +34,7 @@ var calculateIncome = function ()
         }
         else
         {
-            if (floatAnnualIncome >= 30000 && floatAnnualIncome >= 75000)
+            if (floatAnnualIncome >= 30000 && floatAnnualIncome <= 75000)
             {
                 floatTaxRate = 10;
             }
